10b618051b482c9d62c98b0de1451aef232a7421
HALA.. <halahousayan@gmail.com>
2026-04-29 10:07:27 +0300

    Initial commit: E-commerce backend engine with Spring Boot

---
+++ Downloads/H2NS/H2NS/src/main/java/com/EcommerceApp/H2NS/H2NsApplication.java
@@ -0,0 +1,13 @@
+package com.EcommerceApp.H2NS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2NsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2NsApplication.class, args);
+	}
+
+}
